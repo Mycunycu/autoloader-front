e68ad8b9ac17e70bdc32d51a6281a4d5afc0bfe6
Aleksei Deviatovskii <mycunycu2607@gmail.com>
2020-11-19 09:25:02 +0300

    started implement registration form

--- src/views/components/register/registerForm.tsx
+++ src/views/components/register/registerForm.tsx
@@ -1,7 +1,66 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+
+import { TextField, OutlinedInput, FormControl, InputLabel } from '@material-ui/core';
+
+interface IState {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  isShowPassword: boolean;
+}
 
 export const RegisterForm: FC = () => {
+  const [values, setValues] = useState<IState>({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    isShowPassword: false
+  });
+
   return (
-    <p>RegisterForm</p>
+    <div className='registration'>
+      <div className='registration-logo'>
+        <p className='registration-img'>To Do logo</p>
+      </div>
+      <form className='registration-form'>
+        <FormControl>
+          <InputLabel htmlFor='registration-name'>Имя</InputLabel>
+          <OutlinedInput
+            id='registration-name'
+          />
+        </FormControl>
+        {/* <div className='registration-group'>
+          <TextField
+            label='Имя'
+            variant='outlined'
+            required={true}
+          />
+        </div> */}
+        <div className='registration-group'>
+          <TextField
+            label='Почта'
+            variant='outlined'
+            type='email'
+            required={true}
+          />
+        </div>
+        <div className='registration-group'>
+          <TextField
+            label='Пароль'
+            variant='outlined'
+            required={true}
+          />
+        </div>
+        <div className='registration-group'>
+          <TextField
+            label='Подтвердите пароль'
+            variant='outlined'
+            required={true}
+          />
+        </div>
+      </form>
+    </div>
   )
 }
